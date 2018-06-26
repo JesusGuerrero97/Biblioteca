edae5f0eec90fb252924e27792eaa3b8472d146a
DaniaPerez28 <40497153+DaniaPerez28@users.noreply.github.com>
2018-06-26 17:13:30 -0600

    GG

--- src/Controlador/ControladorSucursal.java
+++ src/Controlador/ControladorSucursal.java
@@ -1,9 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Controlador;
+
 import Modelo.ModeloLibro;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,21 +8,103 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import Vista.VistaSucursal;
 import Modelo.ModeloSucursal;
-/**
- *
- * @author Dania
- */
+
+import Vista.MenuPrincipal;
+import Modelo.ModeloMenuPrincipal;
+import java.util.Calendar;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+
 public class ControladorSucursal implements ActionListener, MouseListener{
     private ModeloSucursal modelo;
     private VistaSucursal vista;
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+    
+    public ControladorSucursal(ModeloSucursal modelo, VistaSucursal vista){
+        this.modelo=modelo;
+        this.vista=vista;
+        this.vista.btnEditar.addActionListener(this);
+        this.vista.btnEliminar.addActionListener(this);
+        this.vista.btnAgregar.addActionListener(this);
+        this.vista.btnCancelar.addActionListener(this);
+        this.vista.btnRegresar.addActionListener(this);
+        this.vista.btnBuscar1.addActionListener(this);
     }
+    
+    public void iniciarVista(){
+        vista.pack();
+        vista.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        vista.setAlwaysOnTop( true );
+        vista.setLocationRelativeTo(null);
+        vista.setAlwaysOnTop( false );
+        vista.setVisible(true);
+        vista.setResizable(true);
+        vista.setTitle("Sucursal");
+        transparenciaButton();
+    }
+    
+    public void transparenciaButton(){
+        vista.btnRegresar.setOpaque(false);
+        vista.btnRegresar.setContentAreaFilled(false);
+        vista.btnRegresar.setBorderPainted(false);
+        vista.btnAgregar.setOpaque(false);
+        vista.btnAgregar.setContentAreaFilled(false);
+        vista.btnAgregar.setBorderPainted(false);
+        vista.btnCancelar.setOpaque(false);
+        vista.btnCancelar.setContentAreaFilled(false);
+        vista.btnCancelar.setBorderPainted(false);
+        vista.btnEliminar.setOpaque(false);
+        vista.btnEliminar.setContentAreaFilled(false);
+        vista.btnEliminar.setBorderPainted(false);
+        vista.btnEditar.setOpaque(false);
+        vista.btnEditar.setContentAreaFilled(false);
+        vista.btnEditar.setBorderPainted(false);
+        vista.btnBuscar1.setOpaque(false);
+        vista.btnBuscar1.setContentAreaFilled(false);
+        vista.btnBuscar1.setBorderPainted(false);
+    }
+    public void limpiarVista(){
+        vista.txtIdSucursal.setText("");
+        vista.txtNombre.setText("");
+        vista.txtCorreo.setText("");
+        vista.txtDireccion.setText("");
+        vista.txtTelefono.setText("");
+    }
+
+    
+    public void actionPerformed(ActionEvent evento){
+        if(vista.btnAgregar == evento.getSource()){
+                    modelo.agregarSucursal(Integer.parseInt(vista.txtIdSucursal.getText()), vista.txtNombre.getText(),vista.txtDireccion.getText(),
+                    vista.txtTelefono.getText(),vista.txtCorreo.getText());
+                
+                    JOptionPane.showMessageDialog(vista, "Registro insertado exitosamente");
+                    limpiarVista();
+        }                 
+        else if(vista.btnCancelar == evento.getSource()){
+                limpiarVista();
+            
+        }   
+        else if(vista.btnRegresar == evento.getSource()){
+            MenuPrincipal obj = new MenuPrincipal();
+            ModeloMenuPrincipal modeloMenu = new ModeloMenuPrincipal();
+            //ControladorMenuPrincipal ControladorMenuPrincipal = new ControladorMenuPrincipal(modeloMenu,obj);
+            //ControladorMenuPrincipal.iniciarVista();
+            vista.dispose();
+        }
+    }
+        
 
     @Override
     public void mouseClicked(MouseEvent e) {
--- src/Modelo/ModeloSucursal.java
+++ src/Modelo/ModeloSucursal.java
@@ -4,11 +4,52 @@
  * and open the template in the editor.
  */
 package Modelo;
-
+import Modelo.*;
+import Controlador.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
 /**
  *
  * @author Dania
  */
 public class ModeloSucursal {
+    private int vSucId;
+    private String vSucNom;
+    private String vSucDir;
+    private String vSucTel;
+    private String vSucCor;
+    
+    private Conexion conexion = new Conexion();
+    
+    public void agregarSucursal( int vSucId, String vSucNom, String vSucDir, String vSucTel,String vSucCor)
+    {
+        try
+        {
+            //Para abrir una conxion a la BD
+            Connection con = conexion.abrirConexion();
+            //Para Ejecutar la consulta
+            //Statement s = con.createStatement();
+            //JOptionPane.showMessageDialog(null, vConFecha+"---"+vConHora+"---"+vConTipo+"---"+vConNombre+"---"+vConPeso);
+            String query  = "INSERT INTO sucursal( id_sucursal, nombre_suc, direccion, telefono, coreo) values (?,?,?,?,?)";
+            PreparedStatement preparedStatement = con.prepareStatement(query);
+            preparedStatement.setInt(1,vSucId);
+            preparedStatement.setString(2,vSucNom);
+            preparedStatement.setString(3,vSucDir);
+            preparedStatement.setString(4,vSucTel);
+            preparedStatement.setString(5,vSucCor);
+            preparedStatement.executeUpdate();
+            //JOptionPane.showMessageDialog(null, "Registro agregado");
+            conexion.cerrarConexion(con);
+        }
+        catch(SQLException e)
+        {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+    
     
 }
